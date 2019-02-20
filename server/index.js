86c00a977169b866d90e8258513246056c26a6df
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-02-20 12:20:15 -0600

    revert server to eslint rules

--- server/index.js
+++ server/index.js
@@ -1,55 +1,55 @@
-const express = require('express')
-const next = require('next')
-const path = require('path')
-const expressWS = require('express-ws')
-const secure = require('express-force-https')
-const cookiesMiddleware = require('universal-cookie-express')
-require('dotenv').config()
+const express = require('express');
+const next = require('next');
+const path = require('path');
+const expressWS = require('express-ws');
+const secure = require('express-force-https');
+const cookiesMiddleware = require('universal-cookie-express');
+require('dotenv').config();
 
-const { setup } = require('radiks-server')
-const makeApiController = require('./ApiController')
+const { setup } = require('radiks-server');
+const makeApiController = require('./ApiController');
 
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== 'production';
 
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const port = parseInt(process.env.PORT, 10) || 5000
+const port = parseInt(process.env.PORT, 10) || 5000;
 
 app.prepare().then(async () => {
-  const server = express()
-  server.use(cookiesMiddleware())
-  server.use(secure)
+  const server = express();
+  server.use(cookiesMiddleware());
+  server.use(secure);
 
-  expressWS(server)
+  expressWS(server);
 
-  const RadiksController = await setup()
-  server.use('/radiks', RadiksController)
+  const RadiksController = await setup();
+  server.use('/radiks', RadiksController);
 
   server.use((req, res, _next) => {
     if (!dev && req.host !== 'banter.pub') {
-      console.log('Redirecting from non-production URL:', req.host)
-      return res.redirect('https://banter.pub')
+      console.log('Redirecting from non-production URL:', req.host);
+      return res.redirect('https://banter.pub');
     }
-    return _next()
-  })
+    return _next();
+  });
 
   server.use((req, res, _next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', '*')
-    _next()
-  })
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    _next();
+  });
 
   server.get('/manifest.json', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'static', 'manifest.json'))
-  })
+    res.sendFile(path.join(__dirname, '..', 'static', 'manifest.json'));
+  });
 
-  server.use('/api', makeApiController(RadiksController.db))
+  server.use('/api', makeApiController(RadiksController.db));
 
-  server.get('*', (req, res) => handle(req, res))
+  server.get('*', (req, res) => handle(req, res));
 
   server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
