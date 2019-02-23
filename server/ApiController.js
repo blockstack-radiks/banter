a718364b5029ed83f13e453359b9e6fd369de878
Hank Stoever <hstove@gmail.com>
2019-02-22 20:54:57 -0800

    api method to fetch messages with votes

--- server/ApiController.js
+++ server/ApiController.js
@@ -1,13 +1,64 @@
 const express = require('express');
 const { decorateApp } = require('@awaitjs/express');
+const { COLLECTION } = require('radiks-server/app/lib/constants');
 
 const makeApiController = (db) => {
   const Router = decorateApp(express.Router());
-  // console.log(db);
+  const radiksData = db.collection(COLLECTION);
+
+  Router.getAsync('/messages', async (req, res) => {
+    const match = {
+      $match: {
+        radiksType: 'Message',
+      }
+    };
+    if (req.query.lt) {
+      match.$match.createdAt = {
+        $lt: parseInt(req.query.lt, 10),
+      };
+    }
+    if (req.query.createdBy) {
+      match.$match.createdBy = req.query.createdBy;
+    }
+    const sort = {
+      $sort: { createdAt: -1 }
+    };
+    const limit = {
+      $limit: 10
+    };
+
+    const votesLookup = {
+      $lookup: {
+        from: COLLECTION,
+        localField: '_id',
+        foreignField: 'messageId',
+        as: 'votes'
+      }
+    };
+
+    const pipeline = [match, sort, limit, votesLookup];
+
+    const messages = await radiksData.aggregate(pipeline).toArray();
+
+    const username = req.query.fetcher || req.universalCookies.get('username');
+    messages.forEach((message, index) => {
+      message.hasVoted = false;
+      if (username) {
+        message.votes.forEach((vote) => {
+          if (vote.username === username) {
+            messages[index].hasVoted = true;
+          }
+        });
+      }
+      message.votes = message.votes.length;
+    });
+
+    res.json({ messages });
+  });
 
   Router.getAsync('/avatar/:username', async (req, res) => {
     const { username } = req.params;
-    const user = await db.findOne({ _id: username });
+    const user = await radiksData.findOne({ _id: username });
     let image;
     if (user.profile.image) {
       [image] = user.profile.image;
