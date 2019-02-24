d62b69d5feb7a9b9b468a44491a67b1d5fa7c78b
Hank Stoever <hstove@gmail.com>
2019-02-24 11:03:22 -0800

    better 'from' in emails

--- common/lib/mailer.js
+++ common/lib/mailer.js
@@ -1,6 +1,8 @@
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 
+const FROM = '💩Banter <hello@banter.pub>';
+
 const sendMail = (email) =>
   new Promise(async (resolve, reject) => {
     let transport = null;
@@ -30,7 +32,7 @@ const sendMail = (email) =>
 const mentionedEmail = (html, mention, message) => {
   const url = process.env.STAGING ? 'https://staging.banter.pub' : 'https://banter.pub';
   return {
-    from: 'hello@banter.pub',
+    from: FROM,
     to: mention.email,
     subject: `💩You were mentioned by @${message.createdBy}`,
     html,
@@ -47,7 +49,7 @@ const updatesEmail = (user, messages, html) => {
   const messageLines = messages.map((message) => `@${message.createdBy}: ${message.content}`);
   const url = process.env.STAGING ? 'https://staging.banter.pub' : 'https://banter.pub';
   return {
-    from: 'hello@banter.pub',
+    from: FROM,
     to: user.email,
     subject: `Some 💩from Banter`,
     html,
