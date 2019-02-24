5014f68465b51e7d1a3b420347ad6582e342517e
Hank Stoever <hstove@gmail.com>
2019-02-24 08:16:55 -0800

    nodemon script, show name in email nav always

--- components/email/nav.js
+++ components/email/nav.js
@@ -5,7 +5,7 @@ import { Logo } from '../nav';
 
 export default () => (
   <Flex px={4} py={2} alignItems="center" is="nav" bg="pink">
-    <Box width={1 / 4}>
+    <Box width={1}>
       <Type is="h1" m={0} fontSize="28px" display="inline-block">
         <Type is="a" href="https://banter.pub" color='purple' textDecoration="none">
           <Logo />
