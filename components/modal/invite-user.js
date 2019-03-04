52ebc78c7b66e1d0223e64b504a8c5dea556d174
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-03-04 14:28:06 -0600

    add success screen #44

--- components/modal/invite-user.js
+++ components/modal/invite-user.js
@@ -7,10 +7,12 @@ import Modal from './wrapper';
 import { Button } from '../button';
 import { sendInviteEmails } from '../../common/lib/api';
 import AtIcon from 'mdi-react/AtIcon';
+import Poop from 'mdi-react/EmoticonPoopIcon';
 
 const Content = ({ visible, hide, hasDismissed, show }) => {
   const { lastMentions, lastMessage } = useConnect('selectLastMentions', 'selectLastMessage');
   const [usersToInvite, setUsersToInvite] = useState([]);
+  const [complete, setComplete] = useState(false);
   const [userEmails, setUserEmails] = useState({});
 
   const checkToInvite = async () => {
@@ -100,42 +102,83 @@ const Content = ({ visible, hide, hasDismissed, show }) => {
     if (evt && evt.preventDefault) {
       evt.preventDefault();
     }
-    sendInviteEmails({
-      lastMessage,
-      userEmails,
-    });
-    setUsersToInvite([]);
+    try {
+      const success = await sendInviteEmails({
+        lastMessage,
+        userEmails,
+      });
+      if (success) {
+        console.log('success', success);
+        setComplete(true);
+        setUsersToInvite([]);
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
   };
 
   return (
-    <Box textAlign="center">
-      <Type color="white" m={0} is="h2" maxWidth="60%" lineHeight={1.4}>
-        You mentioned people who haven&apos;t used Banter!
-      </Type>
-
-      <Type color="white" mt={4} lineHeight={1.6} maxWidth="80%">
-        Would you like to invite them via email? Otherwise, they probably won't ever see your message.
-      </Type>
-
-      <form onSubmit={sendInvites}>
-        <Box
-          maxHeight="300px"
-          overflow="auto"
-          borderRadius="4px"
-          mt={5}
-          mb={3}
-          border="1px solid rgb(230, 236, 240)"
-          boxShadow="card"
+    <Box py={4} textAlign="center">
+      {complete ? (
+        <Flex
+          mt={4}
+          mb={4}
+          alignItems="center"
+          justifyContent="center"
+          mx="auto"
+          size={180}
+          borderRadius="100%"
+          opacity={0.15}
+          color="purple"
+          bg="white"
         >
-          {emailInputs}
-        </Box>
+          <Poop size={160} style={{ display: 'block' }} />
+        </Flex>
+      ) : null}
+      <Box>
+        <Type color="white" m={0} is="h2" maxWidth="60%" lineHeight={1.4}>
+          {!complete ? <>You mentioned people who haven&apos;t used Banter!</> : <>Success!</>}
+        </Type>
+      </Box>
 
-        <Box mt={6}>
-          <Button invert mx="auto">
-            Send Invites
+      <Box>
+        {' '}
+        <Type color="white" mt={4} lineHeight={1.6} maxWidth="80%">
+          {!complete ? (
+            <>Would you like to invite them via email? Otherwise, they probably won't ever see your message.</>
+          ) : (
+            <>Your invite(s) have been sent! We'll let them know that you're talking about them.</>
+          )}
+        </Type>
+      </Box>
+
+      {!complete ? (
+        <form onSubmit={sendInvites}>
+          <Box
+            maxHeight="300px"
+            overflow="auto"
+            borderRadius="4px"
+            mt={5}
+            mb={3}
+            border="1px solid rgb(230, 236, 240)"
+            boxShadow="card"
+          >
+            {emailInputs}
+          </Box>
+
+          <Box mt={6}>
+            <Button invert mx="auto">
+              Send Invites
+            </Button>
+          </Box>
+        </form>
+      ) : (
+        <Box mt={5}>
+          <Button onClick={hide} invert mx="auto">
+            Close
           </Button>
         </Box>
-      </form>
+      )}
     </Box>
   );
 };
