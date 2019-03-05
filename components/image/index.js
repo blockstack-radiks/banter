04de67ebf7173d1689002623e4fbd486cb8bbb2d
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-03-05 11:30:01 -0600

    add lazysizes

--- components/image/index.js
+++ components/image/index.js
@@ -1,5 +1,28 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Box } from 'blockstack-ui';
+import styled from 'styled-components';
+
+const ImageWrapper = styled(Box)`
+  img.lazyloaded {
+    opacity: 1 !important;
+  }
+`;
+
+const PreviewImage = ({ ...rest }) => <Box width={1} is="img" display="block" maxWidth="100%" {...rest} />;
+const HighResImage = ({ ...rest }) => (
+  <Box
+    transition="0.5s all ease-in-out"
+    opacity={0}
+    position="absolute"
+    left={0}
+    top={0}
+    width={1}
+    is="img"
+    display="block"
+    maxWidth="100%"
+    {...rest}
+  />
+);
 
 const Image = ({ src, ...rest }) => {
   const isImgix = src.includes('imgix');
@@ -11,9 +34,10 @@ const Image = ({ src, ...rest }) => {
   const preview = src + '?auto=format&w=100&blur=100';
 
   return (
-    <Box width={1}>
-      <Box width={1} is="img" display="block" maxWidth="100%" src={preview} {...rest} />
-    </Box>
+    <ImageWrapper width={1} position="relative">
+      <HighResImage className="lazyload" data-src={highRes} />
+      <PreviewImage src={preview} />
+    </ImageWrapper>
   );
 };
 
