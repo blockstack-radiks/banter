c9b5fd1d5b350398fa058161c0e7f6746c3ebf71
Hank Stoever <hstove@gmail.com>
2019-03-05 11:15:46 -0800

    default email frequency to 'every few hours', fixes #52

--- common/constants.js
+++ common/constants.js
@@ -3,6 +3,6 @@ export const USER_SETTINGS = 'UserSettings';
 export const defaultUserSettings = {
   notifyMentioned: true,
   sendUpdates: true,
-  updateFrequency: 'daily',
+  updateFrequency: 'hours',
   email: '',
 };
