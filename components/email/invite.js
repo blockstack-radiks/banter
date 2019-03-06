7f72793f6de88c3b065d6a303c195b2112c320ea
Thomas Osmonson <thomas.osmonson@gmail.com>
2019-03-06 12:22:43 -0600

    add invite template

---
+++ components/email/invite.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Type } from 'blockstack-ui';
+import Message from '../../models/Message';
+import Layout from './layout';
+import { Block, Message as MessageComponent, Header, Footer } from './shared';
+
+export default ({ message, mention: user }) => {
+  const _message = new Message(message);
+
+  return (
+    <Layout hiddenText={_message.attrs.content}>
+      <Header user={user} title="You've been invited." />
+      <Block pb={4}>
+        <Type color="purple">Someone is talking 💩 about you on Banter!</Type>
+      </Block>
+      <MessageComponent message={_message} isLast />
+      <Footer />
+    </Layout>
+  );
